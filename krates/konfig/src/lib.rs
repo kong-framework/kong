fa3d2bf53fe3ab639e2fa648cdc5c0bc9477f9a4
Jackson G. Kaindume <kaindume@proton.me>
2023-02-12 01:07:38 +0200

    [konfig]: Add konfig

---
+++ krates/konfig/src/lib.rs
@@ -0,0 +1,45 @@
+//! server configuration
+use kerror::KError;
+use serde::Deserialize;
+use std::{env, fs};
+
+/// Server configuration
+#[derive(Deserialize)]
+pub struct Konfig {
+    /// Port to access the server
+    pub port: u16,
+    /// Admin email address
+    pub admin_email: String,
+    /// Admin Accounts database path
+    pub admin_accounts_database: String,
+    /// Name of the authorization session cookie id
+    pub auth_cookie_name: String,
+}
+
+impl Konfig {
+    /// Read server config file from path provided as an argument when
+    /// the program was started.
+    pub fn read() -> Result<Konfig, KError> {
+        let arg = env::args().nth(1);
+        match arg {
+            Some(a) => {
+                let toml_str = fs::read_to_string(a).map_err(|_| KError::ConfigError)?;
+                let config: Konfig = toml::from_str(&toml_str).map_err(|_| KError::ConfigError)?;
+                Ok(config)
+            }
+            None => panic!("Path to config file is not provided!"),
+        }
+    }
+
+    pub fn read_port() -> u16 {
+        let arg = env::args().nth(1);
+        match arg {
+            Some(a) => {
+                let toml_str = fs::read_to_string(a).unwrap();
+                let config: Konfig = toml::from_str(&toml_str).unwrap();
+                config.port
+            }
+            None => panic!("Path to config file is not provided!"),
+        }
+    }
+}
