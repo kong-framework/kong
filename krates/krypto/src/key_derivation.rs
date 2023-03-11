a7c50c11708a3b4c2b90ccf183e2388d41963979
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-03-11 03:34:07 +0200

    [krypto]: implement blake3 based key derivation

---
+++ krates/krypto/src/key_derivation.rs
@@ -0,0 +1,51 @@
+//! Management of cryptographic keys
+
+use chrono::prelude::*;
+
+/// The context of a key derivation
+pub struct Context<'a> {
+    /// Server or application that derives and use the key
+    pub host: &'a str,
+    /// The time when the key is derived
+    pub timestamp: DateTime<Utc>,
+}
+
+impl<'a> Context<'a> {
+    pub fn to_string(&self) -> String {
+        format!("{} {} kpassport-token", self.host, self.timestamp)
+    }
+}
+
+/// Derive a 32 byte key from specified context and
+/// key material
+pub fn derive_key(context: Context, key_material: &[u8]) -> [u8; 32] {
+    blake3::derive_key(&context.to_string(), key_material)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_key_derivation() {
+        let key_material = "my super secure secret key".as_bytes();
+        let context = Context {
+            host: "My Application",
+            timestamp: Utc::now(),
+        };
+
+        let derived_key = derive_key(context, key_material);
+
+        let key_material2 = "secret".as_bytes();
+        let context2 = Context {
+            host: "My Application",
+            timestamp: Utc::now(),
+        };
+
+        let derived_key2 = derive_key(context2, key_material2);
+
+        assert_eq!(derived_key.len(), 32);
+        assert_eq!(derived_key2.len(), 32);
+        assert_ne!(derived_key, derived_key2);
+    }
+}
