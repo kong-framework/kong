5b0da5c66a80ab262087388c81de9f88049c5023
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-04-03 00:23:26 +0200

    [kong]: remove Kontrol trait

--- krates/kong/src/kontrol/mod.rs
+++ krates/kong/src/kontrol/mod.rs
@@ -3,7 +3,6 @@
 //! `kong` API request controllers
 
 use crate::Kong;
-use kdata::inputs::UserInput;
 use rouille::{Request, Response};
 use serde::Serialize;
 pub mod accounts;
@@ -11,17 +10,6 @@ pub mod auth;
 use kerror::KError;
 use route_recognizer::{Params, Router};
 
-/// Trait for an HTTP endpoint /  request handler
-pub trait Kontrol {
-    /// Check request HTTP methods and handle accordingly
-    fn kontrol(kong: &mut Kong, req: &Request) -> Response;
-
-    /// Validate input from user
-    fn validate_user_input(input: impl UserInput) -> bool;
-
-    /// Handle request from a HTTP POST method
-    fn post(kong: &mut Kong, req: &Request) -> Response;
-}
 
 /// Request Kontroller
 pub struct Kontroller<'a> {
