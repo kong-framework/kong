4e6f991371099af27cd71787a11245cbeacb1feb
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-04-14 14:21:35 +0200

    [konfig]: add kong default values

---
+++ krates/konfig/src/defaults.rs
@@ -0,0 +1,7 @@
+//! `kong` default values
+
+/// Kong server working directory
+pub const WORKING_DIRECTORY: &str = "~/kong/";
+
+/// Accounts directory inside the working directory
+pub const ACCONTS_DB: &str = "databases/ACCOUNT.sqlite";
