5a88eec78e273e0f38ae12f6de8a598dadcae2e8
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-04-14 15:02:32 +0200

    [konfig]: add default path for the databases directory

--- krates/konfig/src/defaults.rs
+++ krates/konfig/src/defaults.rs
@@ -2,6 +2,7 @@
 
 /// Kong server working directory
 pub const WORKING_DIRECTORY: &str = "~/kong/";
-
+/// Kong server databases working directory
+pub const DBS_DIRECTORY: &str = "databases/";
 /// Accounts directory inside the working directory
 pub const ACCONTS_DB: &str = "databases/ACCOUNT.sqlite";
