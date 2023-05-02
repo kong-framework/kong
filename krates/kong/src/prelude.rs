7cffcf195890a01e6febd8c89991bda5505b5f0d
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-05-02 11:53:24 +0200

    [kong]: update prelude

--- krates/kong/src/prelude.rs
+++ krates/kong/src/prelude.rs
@@ -1,11 +1,17 @@
 //! Re-exports of upstream crates
 
-pub use crate::kontrol::{accounts::CreateAccountKontroller, Kontrol, Method};
+pub use crate::kontrol::{
+    error_response::ErrorResponse, issue_kpassport::is_admin, Kontrol, Method,
+};
 pub use crate::kroute::kroute;
 pub use crate::Kong;
 pub use kdata;
 pub use kerror::KError;
 pub use kollection::Kollection;
 pub use konfig::Konfig;
-pub use krypto::kpassport;
+pub use krypto;
 pub use rouille as server;
+pub use serde_json;
+pub use serde_json::error::Error as JsonError;
+pub use serde_json::json;
+pub use serde_json::Value as JsonValue;
