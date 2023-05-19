e152074dd033f4333311c5fab2cd866e7720e9e2
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-05-19 08:06:00 +0200

    [kong/init]: implement  initialization

--- krates/kong/src/lib.rs
+++ krates/kong/src/lib.rs
@@ -35,7 +35,7 @@ pub use serde_json::value::Value as JsonValue;
 use krypto::kpassport::Kpassport;
 use rouille::{Request, Response};
 use route_recognizer::{Params, Router};
-use std::str::FromStr;
+use std::{fs::File, str::FromStr};
 
 /// 🔥 Kong object
 pub struct Kong {
@@ -47,10 +47,58 @@ pub struct Kong {
     pub input: Option<serde_json::Value>,
 }
 
+impl Kong {
+    /// Initialize kong, by creating the working directory if it does
+    /// not exist and it content if it does not exist (for example the
+    /// LOG file)
+    fn init(config: &Konfig) {
+        Kong::create_working_directory(config);
+        Kong::create_log_file(config);
+    }
+
+    /// Create working dirctory if it does not already exist
+    fn create_working_directory(config: &Konfig) {
+        // Get path to working directory
+        let working_dir = Konfig::read_working_dir();
+        let working_dir = std::path::Path::new(&working_dir);
+
+        if !std::path::Path::exists(working_dir) {
+            // create working directory
+            // XXX: Note that using unwrap() here is safe, because
+            // this function is a called a start up during the
+            // initialization phase of kong.
+            std::fs::create_dir(working_dir).unwrap()
+        }
+    }
+
+    /// Create log file if file logging is enabled and the LOG
+    /// file does not yet exist.
+    fn create_log_file(config: &Konfig) {
+        //let logging = config.log_file
+        if let Some(file_logging) = config.log_file {
+            if file_logging {
+                let working_dir = Konfig::read_working_dir();
+                let working_dir_path = std::path::Path::new(&working_dir);
+                let log_file = std::path::Path::new(defaults::LOG_FILE);
+                let log_file_path = working_dir_path.join(log_file);
+
+                if !std::path::Path::exists(&log_file_path) {
+                    // create log file in the working directory
+                    // XXX: Note that using unwrap() here is safe, because
+                    // this function is a called a start up during the
+                    // initialization phase of kong.
+                    File::create(log_file_path).unwrap();
+                }
+            }
+        }
+    }
+}
 impl Default for Kong {
     /// Create new kong instance
     fn default() -> Self {
         let config = Konfig::read().expect("Could not read configuration file.");
+
+        Kong::init(&config);
 
         Kong {
             config,
