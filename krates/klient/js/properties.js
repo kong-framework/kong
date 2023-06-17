907b56e1689c78f0fce7e03cdf4ae2d106bc3aea
Jackson G. Kaindume <cy6erlion@protonmail.com>
2023-06-17 20:38:39 +0200

    [klient]: update kong js klient

---
+++ krates/klient/js/properties.js
@@ -0,0 +1,38 @@
+const PropertiesAPI = {
+    address: "/properties",
+    async add_property(notifier){
+	const form_data = new FormData();
+	const name = document.getElementById("PROPERTY_INPUT_NAME").value;
+	const photos = document.getElementById("PROPERTY_INPUT_PHOTOS");
+
+	form_data.append("name",name);
+	let i = 0;
+
+	for (const photo of photos.files) {
+	    form_data.append(`photos_${i}`, photo);
+	    i++;
+	}
+
+	return fetch('/properties', {
+	    method: 'POST',
+	    headers: {
+		'Content-Type': 'application/json',
+	    },
+	    body: form_data,
+	})
+	    .then((response) => {
+		if (response.status === 201){
+		    notifier.success("Property added successfully");
+		}
+		return response.json();
+	    })
+	    .then((data) => {
+		if(data.msg){
+		    notifier.error(data.msg);
+		}
+	    })
+	    .catch((error) => {
+		notifier.error(error)
+	    });
+    }
+}
